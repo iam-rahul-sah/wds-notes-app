8567183f5a2fca216246cb377714dac2687b5a20
Rahul Sah <rahulsah0221@gmail.com>
2024-11-28 06:09:27 +0000

    note creation and tag creation logic done

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,14 +1,16 @@
 import NoteForm from "./NoteForm";
-import { NoteData } from "./App";
+import { NoteData, Tag } from "./App";
 
 type NewNoteProps = {
     onSubmit: (data: NoteData) => void
+    onAddTag: (tag: Tag) => void
+    availableTags: Tag[]
 }
-const NewNote = ({onSubmit}: NewNoteProps) => {
+const NewNote = ({onSubmit, onAddTag, availableTags}: NewNoteProps) => {
     return (
         <>
             <h1 className="mb-4">New Note</h1>
-            <NoteForm onSubmit={onSubmit}/>
+            <NoteForm onSubmit={onSubmit} onAddTag={onAddTag} availableTags={availableTags}/>
         </>
     )
 }
